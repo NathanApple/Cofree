01e51d6baee00828a67c5c743250bbf37ff7166a
Nathaniel C <nathanielcandra2003@gmail.com>
2023-05-30 18:17:10 +0700

    grid bisa di klik

--- src/main/java/com/aol/PrimaryController.java
+++ src/main/java/com/aol/PrimaryController.java
@@ -3,6 +3,7 @@ package com.aol;
 import java.io.IOException;
 import java.lang.System.Logger;
 
+import javafx.application.Application;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
@@ -10,7 +11,14 @@ import javafx.scene.Node;
 import javafx.scene.control.Label;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
+import javafx.scene.input.KeyEvent;
+import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.ColumnConstraints;
 import javafx.scene.layout.GridPane;
+import javafx.scene.layout.Pane;
+import javafx.scene.layout.Priority;
+import javafx.scene.layout.RowConstraints;
+import javafx.stage.Stage;
 
 public class PrimaryController {
 
@@ -21,16 +29,63 @@ public class PrimaryController {
     private Label label1;
 
     @FXML
+    private GridPane cardGrid;
+
+    @FXML
     private void switchToSecondary() throws IOException {
         App.setRoot("secondary");
     }
 
+    @FXML
+    private void clickGrid(MouseEvent event) {
+        // System.out.println("Clicked");
+        // Node source = (Node) event.getTarget();
+        Node source = (Node) event.getSource();
+        System.out.println(event);
+        System.out.println(source);
+        // int columnIndex = GridPane.getColumnIndex(source);
+        // int rowIndex = GridPane.getRowIndex(source);
+        // System.out.println("Row : " + rowIndex + ", Col : " + columnIndex);
+    }
+
+    public void initialize() {
+        int numCols = 4 ;
+        int numRows = 3 ;
+
+        for (int i = 0 ; i < numCols ; i++) {
+            ColumnConstraints colConstraints = new ColumnConstraints();
+            colConstraints.setHgrow(Priority.SOMETIMES);
+            cardGrid.getColumnConstraints().add(colConstraints);
+        }
+
+        for (int i = 0 ; i < numRows ; i++) {
+            RowConstraints rowConstraints = new RowConstraints();
+            rowConstraints.setVgrow(Priority.SOMETIMES);
+            cardGrid.getRowConstraints().add(rowConstraints);
+        }
+
+        for (int i = 0 ; i < numCols ; i++) {
+            for (int j = 0; j < numRows; j++) {
+                addPane(i, j);
+            }
+        }
+    }
+
+    private void addPane(int colIndex, int rowIndex) {
+        Pane pane = new Pane();
+        pane.setOnMouseClicked(e -> {
+            System.out.printf("Mouse enetered cell [%d, %d]%n", colIndex, rowIndex);
+        });
+        cardGrid.add(pane, colIndex, rowIndex);
+    }
+
+
     // @FXML
     // public void buttonPressed(ActionEvent event) {
-    //     Object source = event.getSource();
-    //     System.out.println("TEST");
-    //     System.out.println(source);
-    //     // ...
+    // Object source = event.getSource();
+    // System.out.println("TEST");
+    // System.out.println(source);
+    // // ...
     // }
 
     @FXML
